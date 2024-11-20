f712fe9faf9cf3f0052af18f3974bb375926efa2
Or Levitas <orlevitas@gmail.com>
2024-11-20 12:10:36 +0200

    tmp

---
+++ version2/config.py
@@ -0,0 +1,29 @@
+import os 
+
+# Data locatopns
+DATA_DIR_LOC = os.path.join(os.getcwd(), 'data_dir')
+DATA_DIV_LOC = os.path.join(DATA_DIR_LOC, 'data_dividends.csv')
+DATA_OPEN_LOC = os.path.join(DATA_DIR_LOC, 'data_open.csv')
+DATA_CLOSE_LOC = os.path.join(DATA_DIR_LOC, 'data_close.csv')
+DATA_LOW_LOC = os.path.join(DATA_DIR_LOC, 'data_low.csv')
+DATA_HIGH_LOC = os.path.join(DATA_DIR_LOC, 'data_high.csv')
+DATA_VOL_LOC = os.path.join(DATA_DIR_LOC, 'data_vol.csv')
+DATA_POS_LOC = os.path.join(DATA_DIR_LOC, 'ml_stat.pickle')
+DATA_ML_VIZ_LOC = os.path.join(DATA_DIR_LOC, 'ml_viz_stat.pickle')
+DATA_ADJ_CLOSE_LOC = os.path.join(DATA_DIR_LOC, 'data_close_adj.csv')
+
+IMG_DIR_LOC = os.path.join(os.getcwd(), 'images')
+
+# Momentum hyperparameters
+START_DATE = '2004-01-01'
+END_DATE = '2024-10-01'
+STOCK_TIME = 'Adj Close'
+HOLDING_PERIOD_MONTHS = 3
+FORMATION_PERIOD_MONTHS = 12
+TOP_DECILE = 10
+GROUP_LABELS = {'w_stocks_higher':0, 'w_stocks_lower':1, 'l_stocks_higher':2, 'l_stocks_lower':3}
+GROUP_LABELS_TO_STR = {v: k for k, v in GROUP_LABELS.items()}
+
+# Model hyperparameters
+RANDOM_STATE = 42
+TRAIN_SPLIT = 0.7
